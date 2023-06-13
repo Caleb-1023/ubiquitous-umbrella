a7a64addf39910f719df32aae6a9dd159649943b
Caleb-1023 <okaleb106@gmail.com>
2023-06-13 22:29:51 +0100

    home categories picture change

--- src/components/home/categories.component.jsx
+++ src/components/home/categories.component.jsx
@@ -5,22 +5,22 @@ const Categories = () => {
     <div className="w-screen">
         <div className="w-full max-w-6xl m-auto py-8">
             <h2 className="text-2xl font-bold mb-2">Browse by category</h2>
-            <div className="flex items-center justify-between space-x-10">
-                <Link to={'/products/categories/shoes'} className="text-center border-[1px] rounded">
-                    <img src="/category-graduation.png" alt="" className="rounded-t" />
-                    <p className="p-3">Shoes</p>
+            <div className="grid grid-cols-4 gap-5 my-3">
+                <Link to={'/products/categories/shoes'} className=" text-center border-gray-300 border-[1px] rounded">
+                    <img src="/shoes.jpg" alt="" className="rounded-t h-3/4 object-cover" />
+                    <p className="p-3 h-1/4">Shoes</p>
                 </Link>
-                <Link to={'/products/categories/clothing'} className="text-center border-[1px] rounded">
-                    <img src="/category-furniture.png" alt="" className="rounded-t" />
-                    <p className="p-3">Clothing</p>
+                <Link to={'/products/categories/clothing'} className=" text-center border-gray-300 border-[1px] rounded">
+                    <img src="/clothing.jpg" alt="" className="rounded-t h-3/4 object-cover" />
+                    <p className="p-3 h-1/4">Clothing</p>
                 </Link>
-                <Link to={'/products/categories/accessories'} className="text-center border-[1px] rounded">
-                    <img src="/category-school.png" alt="" className="rounded-t" />
-                    <p className="p-3">Accessories</p>
+                <Link to={'/products/categories/stationery'} className=" text-center border-gray-300 border-[1px] rounded">
+                    <img src="/stationery.jpg" alt="" className="rounded-t h-3/4 object-cover" />
+                    <p className="p-3 h-1/4">Stationery</p>
                 </Link>
-                <Link to={'/products/categories/food'} className="text-center border-[1px] rounded">
-                    <img src="/category-electronics.png" alt="" className="rounded-t" />
-                    <p className="p-3">Food</p>
+                <Link to={'/products/categories/food'} className=" text-center border-gray-300 border-[1px] rounded">
+                    <img src="/food.jpg" alt="" className="rounded-t h-3/4 object-cover" />
+                    <p className="p-3 h-1/4">Food</p>
                 </Link>
             </div>
         </div>
